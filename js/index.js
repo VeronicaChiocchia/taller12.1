88c9087fc97f04e591cd4009b9b9cdf37211b82a
VeronicaChiocchia <veronicachiocchia@gmail.com>
2024-09-26 15:26:50 -0300

    funcionalidad

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const movieCatalog = "https://japceibal.github.io/japflix_api/movies-data.json";
 const inputBuscar = document.getElementById("inputBuscar");
 const btnBuscar = document.getElementById("btnBuscar");
 const listaUl = document.getElementById("lista");
+let listItems = document.querySelectorAll('li');
 let movieList = [];
 
 function getJSONData() {
@@ -13,7 +14,6 @@ function getJSONData() {
       return response.json();
     })
     .then((data) => {
-      // console.log(data[0].budget); // Logs the data received from the API;
       movieList.push(...data);
       console.log(movieList);
     })
@@ -28,47 +28,57 @@ function cleanList() {
   listaUl.innerHTML = "";
 }
 
-function creatListItem(item) {
-  const newItem = document.createElement("div");
-  // const starRating = item.vote_average/2;
+function createListItem(item) {
+  const newItem = document.createElement("li");
+  newItem.classList.add(
+    "list-group-item",
+    "list-group-item-action",
+    "bg-dark",
+    "list-item",
+    "text-light"
+  );
+
+  newItem.setAttribute('data-bs-toggle', 'offcanvas'); 
+  newItem.setAttribute('data-bs-target', '#offcanvasTop'); 
+  newItem.setAttribute('aria-controls', 'offcanvasTop');
+
   let HTMLtoAppend = `
-    <li class="list-group-item list-group-item-action">
-            <div class="container row">
+            <div class="col">
               <div class="col">
                 <div class="col movie-title">${item.title}</div>
-                <div class="col movie-overview">${item.overview}</div>
-              </div>
-              <div class="col-auto star-rating">
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
+                <div class="col movie-overview">${item.tagline}</div>
               </div>
             </div>
-          </li>`;
+          `;
+
   const listElement = listaUl.appendChild(newItem);
   listElement.innerHTML = HTMLtoAppend;
-  // const stars = document.getElementsByClassName('star-rating')[0].children;
-  // const starsArray = Array.from(stars);
-  // for (let star of starsArray) {
-  //   if (starsArray.indexOf(star) <= starRating - 1) {
-  //     star.classList.toggle("checked");
-  //   }
-  // }
-}
 
-function setStarRating(item) {
-  let stars = document.getElementsByClassName('star-rating')[0].children;
-  const starsArray = Array.from(stars);
-  const starRating = item.vote_average/2;
-  for (let star of starsArray) {
-    if (starsArray.indexOf(star) <= starRating - 1) {
-      star.classList.add('checked');
-    } else {
-      star.classList.remove('checked');
-    }
+  const starRating = item.vote_average / 2;
+  const starsContainer = document.createElement("div");
+  starsContainer.classList.add("stars-container", "star-rating");
+  newItem.appendChild(starsContainer);
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("i");
+    star.className = i <= starRating ? "fas fa-star" : "far fa-star"; // Solid star or outlined star
+    starsContainer.appendChild(star);
   }
+
+  newItem.addEventListener('click', function() {
+    let offCanvasTitle = document.getElementById('offcanvasTopLabel');
+    let offcanvasOverview = document.getElementById('offcanvasOverview');
+    let offcanvasGenres = document.getElementById('offcanvasGenres');
+    offCanvasTitle.innerText = item.title;
+    offcanvasOverview.innerText = item.overview;
+    console.log(item.genres);
+    let genres = "";
+    for (let genre of item.genres) {
+      genres += genre;
+    }
+    console.log(genres);
+    
+  });
+
 }
 
 btnBuscar.addEventListener("click", () => {
@@ -81,10 +91,20 @@ btnBuscar.addEventListener("click", () => {
   // Corroboramos si hay coincidencia:
 
   for (let item of movieList) {
-    if (regex.test(item.title)) {
-      // console.log("El título sí está:" + item.title);
-      creatListItem(item);
-      setStarRating(item);
+    if (regex.test(item.title) || regex.test(item.genres) || regex.test(item.tagline) || regex.test(item.overview)) {
+      createListItem(item);
     }
   }
+});
+
+listItems.forEach(function(item) {
+  // Add an event listener to each <li>
+  item.addEventListener('click', function() {
+    let offCanvasTitle = document.getElementById('offcanvasTopLabel');
+    console.log(item);
+    // item.setAttribute('data-bs-toggle', 'offcanvas'); 
+    // item.setAttribute('data-bs-target', '#offcanvasTop'); 
+    // item.setAttribute('aria-controls', 'offcanvasTop');
+      alert('You clicked on: ' + item.textContent);
+  });
 });
